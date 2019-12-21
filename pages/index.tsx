47fdaa08a4d341c1db2d0b5534f228ed687e0289
sduwangtong <watng@amazon.com>
2019-12-21 17:45:33 -0500

    function component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NextPage } from 'next';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import { Layout } from '../components/Layout';
 
 interface IntialProps {
     greeting: string;
@@ -31,27 +32,24 @@ interface TasksQueryVariable {
 }
 const IndexPage: NextPage<Props, IntialProps> = props => {
     return (
-    <Query<TasksQuery, TasksQueryVariable> query={taskQuery} variables={{status:"completed"}}> 
-        {({loading, error, data}) => {
-            if (loading) {
-                return <p> loading </p>
-            } else if (error) {
-                return <p>error</p>
-            } 
-            const tasks = data && data.tasks ? data.tasks : [];
-            
-        
-            return <ul> { tasks.map(task => {
-                return <li key={task.id}> {task.title} </li>
-            })}</ul>
+        <Layout> 
+            <Query<TasksQuery, TasksQueryVariable> query={taskQuery} variables={{status:"completed"}}> 
+                {({loading, error, data}) => {
+                    if (loading) {
+                        return <p> loading </p>
+                    } else if (error) {
+                        return <p>error</p>
+                    } 
+                    const tasks = data && data.tasks ? data.tasks : [];
+                    
+                
+                    return <ul> { tasks.map(task => {
+                        return <li key={task.id}> {task.title} </li>
+                    })}</ul>
 
-        }}  
-    </Query>
+                }}  
+            </Query>
+        </Layout>
     );
 };
-
-IndexPage.getInitialProps = async() => ({
-    greeting: "Hi"
-});
-
 export default IndexPage;
