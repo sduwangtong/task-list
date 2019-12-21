171ca3db4023bdc15c60e69c573aa63d39d99715
sduwangtong <watng@amazon.com>
2019-12-21 10:53:18 -0500

    graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NextPage } from 'next';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
 interface IntialProps {
     greeting: string;
@@ -8,8 +10,44 @@ interface IntialProps {
 interface Props extends IntialProps {
 
 }
+
+const taskQuery = gql`query Tasks($status: TaskStatus) {
+    tasks(status: $status) {
+      id
+      title
+      status
+    }
+  }`;
+interface TasksQuery {
+    tasks: {
+        id: number;
+        title: string;
+        status: string;
+    }[];
+}
+
+interface TasksQueryVariable {
+    status: string;
+}
 const IndexPage: NextPage<Props, IntialProps> = props => {
-    return <div>{props.greeting}</div>;
+    return (
+    <Query<TasksQuery, TasksQueryVariable> query={taskQuery} variables={{status:"completed"}}> 
+        {({loading, error, data}) => {
+            if (loading) {
+                return <p> loading </p>
+            } else if (error) {
+                return <p>error</p>
+            } 
+            const tasks = data && data.tasks ? data.tasks : [];
+            
+        
+            return <ul> { tasks.map(task => {
+                return <li key={task.id}> {task.title} </li>
+            })}</ul>
+
+        }}  
+    </Query>
+    );
 };
 
 IndexPage.getInitialProps = async() => ({
